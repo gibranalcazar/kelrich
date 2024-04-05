a64200fadc8a1d8940587bd7bf0a3a3be20e42cb
gibranalcazar <gibran@live.com>
2024-04-04 22:39:01 -0600

    S5

---
+++ index.js
@@ -0,0 +1,10 @@
+function cerrarMenu() {
+    document.querySelector('.navbar-collapse').classList.remove('show');
+  }
+
+  document.addEventListener('click', function (event) {
+    var isClickInside = document.querySelector('.navbar-collapse').contains(event.target);
+    if (!isClickInside) {
+      document.querySelector('.navbar-collapse').classList.remove('show');
+    }
+  });
